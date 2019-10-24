036291f80b9795357e0a541fee337508618efc69
wilson <l7960261@gmail.com>
2019-10-24 15:07:10 +0800

    Modify tsconfig.json 'compilerOptions' > 'paths'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
   },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'auth', loadChildren: () => import('@auth/auth.module').then(m => m.AuthModule) },
 ];
 
 @NgModule({
