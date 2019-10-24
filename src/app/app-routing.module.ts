381537576ddbd640ff9e3968ef328cb3288552a7
wilson <l7960261@gmail.com>
2019-10-24 14:07:08 +0800

    Add login.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BaseLayoutComponent } from './base-layout/base-layout.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: BaseLayoutComponent,
-  //   children: [
-  //     { path: '', component: HomeComponent },
-  //   ],
-  // },
-  { path: '', component: HomeComponent }
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './base-layout/base-layout.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BaseLayoutComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
